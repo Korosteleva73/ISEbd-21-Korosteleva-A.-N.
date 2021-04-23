c80920f48c4cdaa27a6bb7f7ae38d624cccb3614
Anastasia Korosteleva <nastya.korosteleva00@hotmail.com>
2021-04-23 22:18:17 +0400

    fix

--- CarFactoryListImplement/Implements/OrderStorage.cs
+++ CarFactoryListImplement/Implements/OrderStorage.cs
@@ -34,131 +34,129 @@ namespace CarFactoryListImplement.Implements
             List<OrderViewModel> result = new List<OrderViewModel>();
             foreach (var order in source.Orders)
             {
-                if (order.CarId == model.CarId)
+                if (order.CarId == model.CarId &&
+                    ((!model.DateFrom.HasValue && !model.DateTo.HasValue && order.DateCreate.Date == model.DateCreate.Date) ||
+                     (model.DateFrom.HasValue && model.DateTo.HasValue && order.DateCreate.Date >= model.DateFrom.Value.Date && order.DateCreate.Date <= model.DateTo.Value.Date) ||
+                     (model.ClientId.HasValue && order.ClientId == model.ClientId) ||
+                     (model.FreeOrders.HasValue && model.FreeOrders.Value && order.Status == OrderStatus.Принят) ||
+                     (model.ImplementerId.HasValue && order.ImplementerId == model.ImplementerId && order.Status == OrderStatus.Выполняется)))
                 {
-                    if ((!model.DateFrom.HasValue && !model.DateTo.HasValue && order.DateCreate.Date == model.DateCreate.Date) ||
-                         (model.DateFrom.HasValue && model.DateTo.HasValue && order.DateCreate.Date >= model.DateFrom.Value.Date && order.DateCreate.Date <= model.DateTo.Value.Date) ||
-                         (model.ClientId.HasValue && order.ClientId == model.ClientId) ||
-                         (model.FreeOrders.HasValue && model.FreeOrders.Value && order.Status == OrderStatus.Принят) ||
-                         (model.ImplementerId.HasValue && order.ImplementerId == model.ImplementerId && order.Status == OrderStatus.Выполняется))
-                    {
-                        result.Add(CreateModel(order));
-                    }
+                    result.Add(CreateModel(order));
                 }
-            
+            }
             return result;
         }
-        public OrderViewModel GetElement(OrderBindingModel model)
-        {
-            if (model == null)
+            public OrderViewModel GetElement(OrderBindingModel model)
             {
+                if (model == null)
+                {
+                    return null;
+                }
+                foreach (var order in source.Orders)
+                {
+                    if (order.Id == model.Id || order.CarId ==
+                    model.CarId)
+                    {
+                        return CreateModel(order);
+                    }
+                }
                 return null;
             }
-            foreach (var order in source.Orders)
+            public void Insert(OrderBindingModel model)
             {
-                if (order.Id == model.Id || order.CarId ==
-                model.CarId)
+                Order tempOrder = new Order
                 {
-                    return CreateModel(order);
-                }
-            }
-            return null;
-        }
-        public void Insert(OrderBindingModel model)
-        {
-            Order tempOrder = new Order
-            {
-                Id = 1
-            };
-            foreach (var order in source.Orders)
-            {
-                if (order.Id >= tempOrder.Id)
+                    Id = 1
+                };
+                foreach (var order in source.Orders)
                 {
-                    tempOrder.Id = order.Id + 1;
+                    if (order.Id >= tempOrder.Id)
+                    {
+                        tempOrder.Id = order.Id + 1;
+                    }
                 }
+                source.Orders.Add(CreateModel(model, tempOrder));
             }
-            source.Orders.Add(CreateModel(model, tempOrder));
-        }
-        public void Update(OrderBindingModel model)
-        {
-            Order tempOrder = null;
-            foreach (var order in source.Orders)
+            public void Update(OrderBindingModel model)
             {
-                if (order.Id == model.Id)
+                Order tempOrder = null;
+                foreach (var order in source.Orders)
                 {
-                    tempOrder = order;
+                    if (order.Id == model.Id)
+                    {
+                        tempOrder = order;
+                    }
                 }
+                if (tempOrder == null)
+                {
+                    throw new Exception("Элемент не найден");
+                }
+                CreateModel(model, tempOrder);
             }
-            if (tempOrder == null)
+            public void Delete(OrderBindingModel model)
             {
+                for (int i = 0; i < source.Orders.Count; ++i)
+                {
+                    if (source.Orders[i].Id == model.Id)
+                    {
+                        source.Orders.RemoveAt(i);
+                        return;
+                    }
+                }
                 throw new Exception("Элемент не найден");
             }
-            CreateModel(model, tempOrder);
-        }
-        public void Delete(OrderBindingModel model)
-        {
-            for (int i = 0; i < source.Orders.Count; ++i)
+            private Order CreateModel(OrderBindingModel model, Order order)
             {
-                if (source.Orders[i].Id == model.Id)
-                {
-                    source.Orders.RemoveAt(i);
-                    return;
-                }
+                order.CarId = model.CarId;
+                order.Count = model.Count;
+                order.ClientId = model.ClientId.Value;
+                order.ImplementerId = model.ImplementerId;
+                order.Sum = model.Sum;
+                order.Status = model.Status;
+                order.DateCreate = model.DateCreate;
+                order.DateImplement = model.DateImplement;
+                return order;
             }
-            throw new Exception("Элемент не найден");
-        }
-        private Order CreateModel(OrderBindingModel model, Order order)
-        {
-            order.CarId = model.CarId;
-            order.Count = model.Count;
-            order.ClientId = model.ClientId.Value;
-            order.ImplementerId = model.ImplementerId;
-            order.Sum = model.Sum;
-            order.Status = model.Status;
-            order.DateCreate = model.DateCreate;
-            order.DateImplement = model.DateImplement;
-            return order;
-        }
-        private OrderViewModel CreateModel(Order order)
-        {
-            string carName = "";
-            for (int i = 0; i < source.Cars.Count; ++i)
+            private OrderViewModel CreateModel(Order order)
             {
-                if (source.Cars[i].Id == order.CarId)
+                string carName = "";
+                for (int i = 0; i < source.Cars.Count; ++i)
                 {
-                    carName = source.Cars[i].CarName;
+                    if (source.Cars[i].Id == order.CarId)
+                    {
+                        carName = source.Cars[i].CarName;
+                    }
                 }
-            }
-            string clientFIO = "";
-            for (int i = 0; i < source.Clients.Count; ++i)
-            {
-                if (source.Clients[i].Id == order.ClientId)
+                string clientFIO = "";
+                for (int i = 0; i < source.Clients.Count; ++i)
                 {
-                    clientFIO = source.Clients[i].ClientFIO;
+                    if (source.Clients[i].Id == order.ClientId)
+                    {
+                        clientFIO = source.Clients[i].ClientFIO;
+                    }
                 }
-            }
-            string implementerFIO = "";
-            for (int i = 0; i < source.Implementers.Count; ++i)
-                if (source.Implementers[i].Id == order.ImplementerId)
-                {
-                    clientFIO = source.Implementers[i].FIO;
-                }
+                string implementerFIO = "";
+                for (int i = 0; i < source.Implementers.Count; ++i)
+                    if (source.Implementers[i].Id == order.ImplementerId)
+                    {
+                        clientFIO = source.Implementers[i].FIO;
+                    }
 
-            return new OrderViewModel
-            {
-                Id = order.Id,
-                CarId = order.CarId,
-                CarName = carName,
-                ClientId = order.ClientId,
-                ClientFIO = clientFIO,
-                ImplementerId = order.ImplementerId,
-                ImplementerFIO = implementerFIO,
-                Count = order.Count,
-                Sum = order.Sum,
-                Status = order.Status,
-                DateCreate = order.DateCreate,
-                DateImplement = order.DateImplement,
-            };
+                return new OrderViewModel
+                {
+                    Id = order.Id,
+                    CarId = order.CarId,
+                    CarName = carName,
+                    ClientId = order.ClientId,
+                    ClientFIO = clientFIO,
+                    ImplementerId = order.ImplementerId,
+                    ImplementerFIO = implementerFIO,
+                    Count = order.Count,
+                    Sum = order.Sum,
+                    Status = order.Status,
+                    DateCreate = order.DateCreate,
+                    DateImplement = order.DateImplement,
+                };
+            }
         }
     }
-}
